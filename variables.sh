e160d2d1c6f5c2c326fe0e819d8705db193c1756
Lucas <lucasoliveira2306@gmail.com>
2018-09-11 16:45:12 -0300

    Adding script to set environment variables.

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+DISPLAY=:0.0 ; export DISPLAY xhost +
+
+adb reverse tcp:8081 tcp:8081
+
+echo 256 | tee -a /proc/sys/fs/inotify/max_user_instances
+echo 32768 |  tee -a /proc/sys/fs/inotify/max_queued_events
+echo 65536 |  tee -a /proc/sys/fs/inotify/max_user_watches
