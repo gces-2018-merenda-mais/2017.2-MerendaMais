2a4c8e6767caf6e3ee6857c6d7b4dce5b28c19ed
gustavo2795 <gustavo.unb27@gmail.com>
2018-11-06 10:06:22 -0200

    Creating LawNumber12982 style file

---
+++ src/Styles/LegislationStyles/LawNumber12982Styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    title: {
+      textAlign: 'center',
+      fontSize: 22,
+      fontWeight: '300',
+      marginBottom: 20,
+    },
+    selectors: {
+      marginBottom: 10,
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    selector: {
+      backgroundColor: '#F5FCFF',
+      padding: 10,
+    },
+    activeSelector: {
+      fontWeight: 'bold',
+    },
+    selectTitle: {
+      fontSize: 14,
+      fontWeight: '500',
+      padding: 10,
+    },
+});
+
+export default styles;
+  
