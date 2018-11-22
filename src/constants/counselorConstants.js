b58f3f48b963165cd0833c825e1d705231a7b75b
Hugo Alves <hugo.asb96@gmail.com>
2018-11-22 00:13:59 -0200

    Creating counselor constants file.

---
+++ src/constants/counselorConstants.js
@@ -0,0 +1,17 @@
+// Counselor segment options.
+export const EXECUTIVE_POWER = 'Poder executivo';
+export const EDUCATION_WORKERS = 'Trabalhadores da educação';
+export const STUDENT_PARENTS = 'Pais de alunos';
+export const CIVILIAN_ENTITIES = 'Entidades civis';
+
+// Counselor type options.
+export const TITULAR_COUNSELOR = 'Titular';
+export const SURROGATE_COUNSELOR = 'Suplente';
+
+// Counselor CAE_Type options.
+export const MUNICIPAL_COUNSELOR_CAE = 'Municipal';
+export const STATE_COUNSELOR_CAE = 'Estadual';
+
+// Counselor role options.
+export const PRESIDENT_COUNSELOR = 'Presidente';
+export const COMMON_COUNSELOR = 'Conselheiro';
--- src/constants/generalConstants.js
+++ src/constants/generalConstants.js
@@ -1,21 +1,3 @@
-// Counselor segment options.
-export const EXECUTIVE_POWER = 'Poder executivo';
-export const EDUCATION_WORKERS = 'Trabalhadores da educação';
-export const STUDENT_PARENTS = 'Pais de alunos';
-export const CIVILIAN_ENTITIES = 'Entidades civis';
-
-// Counselor type options.
-export const TITULAR_COUNSELOR = 'Titular';
-export const SURROGATE_COUNSELOR = 'Suplente';
-
-// Counselor CAE_Type options.
-export const MUNICIPAL_COUNSELOR_CAE = 'Municipal';
-export const STATE_COUNSELOR_CAE = 'Estadual';
-
-// Counselor role options.
-export const PRESIDENT_COUNSELOR = 'Presidente';
-export const COMMON_COUNSELOR = 'Conselheiro';
-
 // Responses to checklists in Nuvem.
 export const UNAUDITED = 'Não fiscalizado';
 export const YES = 'Sim';
