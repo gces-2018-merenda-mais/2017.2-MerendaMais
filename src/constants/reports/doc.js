19bbcbedd051c09af050f91c7a9b4283eddcc5c1
Andre Filho <andre.filho001@outlook.com>
2018-12-04 21:03:42 -0200

    Fixes lint errors at src/constants

--- src/constants/reports/doc.js
+++ src/constants/reports/doc.js
@@ -1,13 +1,31 @@
 const doc = [
-  { question: 'DO01', label: 'Ausência de registros de imunização', markedYes: false, markedNo: false, status: false, key: 0 },
-  { question: 'DO02', label: 'Ausência de registro de higienização dos utensílios', markedYes: false, markedNo: false, status: false, key: 1 },
-  { question: 'DO03', label: 'Ausência de registro de capacitação ou treinamento de Merendeiras', markedYes: false, markedNo: false, status: false, key: 2 },
-  { question: 'DO04', label: 'Ausência de registro de portabilidade da água', markedYes: false, markedNo: false, status: false, key: 3 },
-  { question: 'DO05', label: 'Ausência de registro de exames periódicos dos manipuladores', markedYes: false, markedNo: false, status: false, key: 4 },
-  { question: 'DO06', label: 'Ausência de comprovante de limpeza da caixa d’água potável (semestral)', markedYes: false, markedNo: false, status: false, key: 5 },
-  { question: 'DO07', label: 'Ausência de comprovante de limpeza do reservatório de água subterrâneo (semestral)', markedYes: false, markedNo: false, status: false, key: 6 },
-  { question: 'DO08', label: 'Ausência de comprovante de limpeza da caixa de gordura (quinzenal)', markedYes: false, markedNo: false, status: false, key: 7 },
-  { question: 'DO09', label: 'Ausência de comprovante de desinsetização e desratização (semestral)', markedYes: false, markedNo: false, status: false, key: 8 },
+  {
+    question: 'DO01', label: 'Ausência de registros de imunização', markedYes: false, markedNo: false, status: false, key: 0,
+  },
+  {
+    question: 'DO02', label: 'Ausência de registro de higienização dos utensílios', markedYes: false, markedNo: false, status: false, key: 1,
+  },
+  {
+    question: 'DO03', label: 'Ausência de registro de capacitação ou treinamento de Merendeiras', markedYes: false, markedNo: false, status: false, key: 2,
+  },
+  {
+    question: 'DO04', label: 'Ausência de registro de portabilidade da água', markedYes: false, markedNo: false, status: false, key: 3,
+  },
+  {
+    question: 'DO05', label: 'Ausência de registro de exames periódicos dos manipuladores', markedYes: false, markedNo: false, status: false, key: 4,
+  },
+  {
+    question: 'DO06', label: 'Ausência de comprovante de limpeza da caixa d’água potável (semestral)', markedYes: false, markedNo: false, status: false, key: 5,
+  },
+  {
+    question: 'DO07', label: 'Ausência de comprovante de limpeza do reservatório de água subterrâneo (semestral)', markedYes: false, markedNo: false, status: false, key: 6,
+  },
+  {
+    question: 'DO08', label: 'Ausência de comprovante de limpeza da caixa de gordura (quinzenal)', markedYes: false, markedNo: false, status: false, key: 7,
+  },
+  {
+    question: 'DO09', label: 'Ausência de comprovante de desinsetização e desratização (semestral)', markedYes: false, markedNo: false, status: false, key: 8,
+  },
 ];
 
 export default doc;
--- src/constants/reports/waterSewerSupply.js
+++ src/constants/reports/waterSewerSupply.js
@@ -1,7 +1,13 @@
 const waterSewerSupply = [
-  { question: 'WS01', label: 'Reservatório de água e encanamentos inacessíveis, sem tampas, em insatisfatória condição de uso, com vazamentos, infiltrações e descascamentos', markedYes: false, markedNo: false, status: false, key: 0 },
-  { question: 'WS02', label: 'Fossas e esgotos não conectados à rede pública', markedYes: false, markedNo: false, status: false, key: 1 },
-  { question: 'WS03', label: 'Caixas de gordura em inadequado estado de conservação e funcionamento', markedYes: false, markedNo: false, status: false, key: 2 },
+  {
+    question: 'WS01', label: 'Reservatório de água e encanamentos inacessíveis, sem tampas, em insatisfatória condição de uso, com vazamentos, infiltrações e descascamentos', markedYes: false, markedNo: false, status: false, key: 0,
+  },
+  {
+    question: 'WS02', label: 'Fossas e esgotos não conectados à rede pública', markedYes: false, markedNo: false, status: false, key: 1,
+  },
+  {
+    question: 'WS03', label: 'Caixas de gordura em inadequado estado de conservação e funcionamento', markedYes: false, markedNo: false, status: false, key: 2,
+  },
 ];
 
 export default waterSewerSupply;
